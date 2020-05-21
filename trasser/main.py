ea02fa0ff3d130126b92c899e286509772c175cc
Loïc Grobol <loic.grobol@gmail.com>
2020-05-21 11:19:40 +0200

    add conll and a skeletal tei

--- trasser/main.py
+++ trasser/main.py
@@ -2,10 +2,19 @@ import click
 import lxml  # nosec
 import lxml.etree  # nosec
 
+from typing import List, Optional
 
-@click.command()
+import trasser.tei
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
 @click.argument('inpt', nargs=-1)
-def main_entry_point(inpt):
+def tsv(inpt):
     """Convert Transcriber files to raw text"""
     for f in inpt:
         tree = lxml.etree.parse(f)  # nosec
@@ -15,5 +24,58 @@ def main_entry_point(inpt):
                     click.echo(t.strip())
 
 
+@cli.command()
+@click.argument('inpt', nargs=-1)
+@click.option("--attr-blacklist", help="A comma-separated list of attr:value to ignore")
+@click.option(
+    "--lines-xpath",
+    default=".//text/body//p|.//text/body//l|.//text/body//head",
+    help="An xpath expression matching the individual lines to extract",
+)
+@click.option("--tags-blacklist", help="A comma-separated list of tags to ignore")
+def tei(
+    inpt: List[str],
+    lines_xpath: str,
+    tags_blacklist: Optional[str],
+    attr_blacklist: Optional[str],
+):
+    """Convert TEI files to raw text"""
+    if tags_blacklist is None:
+        blacklist_set = set()
+    else:
+        blacklist_set = set(tags_blacklist.split(","))
+
+    def blacklist(elt):
+        return elt.tag in blacklist_set
+
+    if attr_blacklist is None:
+        forbidden_attrs = None
+    else:
+        forbidden_attrs = [tuple(p.split(":")) for p in attr_blacklist.split(",")]
+
+    for f in inpt:
+        tree = lxml.etree.parse(f)  # nosec
+        for e in tree.xpath(lines_xpath):
+            if forbidden_attrs is not None:
+                if any(
+                    p.tag in blacklist_set
+                    or any(p.get(a) == v for a, v in forbidden_attrs)
+                    for p in e.iterancestors()
+                ):
+                    continue
+            click.echo(trasser.tei.get_text(e, blacklist))
+
+
+@cli.command()
+@click.argument('inpt', nargs=-1)
+def conll(inpt):
+    """Convert conll files to raw text"""
+    for f in inpt:
+        with open(f) as in_stream:
+            for l in in_stream:
+                if l.startswith("# text = "):
+                    click.echo(l[9:].strip())
+
+
 if __name__ == '__main__':
-    main_entry_point()
+    cli()
---
+++ trasser/tei.py
@@ -0,0 +1,36 @@
+from __future__ import annotations
+
+import re
+
+from typing import Callable, List, Optional
+
+from lxml import etree  # nosec
+
+
+def get_text(
+    elt: etree._Element, blacklist: Optional[Callable[[etree._Element], bool]]
+) -> str:
+    """Extract the text from an XML subtree, with optional element tag blacklisting
+
+    CAVEAT: this collapses all whitespace to a single " "
+    
+    Note: if you don't actually need a blacklist `etree._Element.itertext` might be a better idea
+    """
+
+    if blacklist is None:
+        blacklist = lambda x: False  # noqa
+
+    def aux(elt: etree._Element) -> List[str]:
+        texts = []
+        if not blacklist(elt):  # type: ignore (https://github.com/python/mypy/issues/2608)
+            if elt.text is not None and not elt.text.isspace():
+                texts.append(elt.text.strip())
+            for child in elt:
+                texts.extend(aux(child))
+        # The tail is not part of the node, so don't ignore it !
+        if elt.tail is not None and not elt.tail.isspace():
+            texts.append(elt.tail.strip())
+        return texts
+
+    texts = aux(elt)
+    return re.sub(r"\s+", " ", " ".join(texts).replace("\n", " "))
